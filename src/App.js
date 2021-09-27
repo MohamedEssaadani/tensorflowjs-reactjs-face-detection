8b72539c86c2d24bba6f2d251a2983cf2fde4162
Mohamed Es-saadani <essaadani80@gmail.com>
2021-09-27 23:32:47 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { useRef } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
 
 function App() {
+  // Setup references
+  const webcamRef = useRef(null);
+  const canvasref = useRef(null);
+
+  // Load facemesh
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.0,
+    });
+  };
+
+  // Define detect function
+  const detect = async (net) => {
+    // check for webcam is running
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readState === 4
+    ) {
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        ></Webcam>
+
+        <canvas
+          ref={canvasref}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        ></canvas>
       </header>
     </div>
   );
